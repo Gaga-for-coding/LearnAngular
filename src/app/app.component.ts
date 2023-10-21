5975f81ef37f51b49bc37cc518b1060321e223b1
Henri-joel Guissi <j.guissi@fintechandpaymentsinstitute.com>
2023-10-21 03:42:56 +0000

    Two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { WishItem } from 'src/shared/models/WishItem';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  items: WishItem[] = [
-          // new WishItem("Buy a Nissan Almera"),
-          // new WishItem("Buy a High end Machine", false)
-  ]
-  title = 'wishList';
+export class AppComponent implements OnInit {
+	items: WishItem[] = [
+		new WishItem('Buy a Nissan Almera'),
+		new WishItem('Buy a High end Machine', true)
+	];
+	title = 'wishList';
+    newWish: any
 
-    ngOnInit(): void {
-    }
+	ngOnInit(): void {
+	}
+
+	addWish() {
+      this.items.push(new WishItem(this.newWish))
+      this.newWish = ''
+	}
+
+	toggleItem(item: any) {
+		item.isComplete = !item.isComplete;
+		console.log(item);
+	}
 }
